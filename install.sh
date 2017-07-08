590979cd577f7767fd6e5f621ba9452b132c7003
romibi <romibi@bluewin.ch>
2017-07-08 13:24:47 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd python-uinput
 python setup.py install
+rm -R build
 cd ../py-spidev
 python setup.py install
-# todo add joystick.py to rc.local
+cd ../
+
+installed=`grep joystick.py /etc/rc.local`
+if [ "$installed" == "" ]; then
+    sed -i '/^exit 0/i\'`pwd`'/joystick.py &' /etc/rc.local
+fi
